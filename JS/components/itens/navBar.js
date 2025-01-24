d38f2405cede13197c1c16a2ad684591a2ae5ba1
Kawã Kleber <kawaklebersc@gmail.com>
2025-01-24 00:10:20 -0300

    Seven Commit

--- JS/components/itens/navBar.js
+++ JS/components/itens/navBar.js
@@ -1,10 +1,15 @@
 const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobileMenu');
+const menuLinks = mobileMenu.querySelectorAll('a'); // Seleciona todos os links dentro do menu móvel
 
+// Alternar o menu ao clicar no botão de hambúrguer
 hamburger.addEventListener('click', () => {
-    if (mobileMenu.classList.contains('open')) {
+    mobileMenu.classList.toggle('open');
+});
+
+// Fechar o menu ao clicar em qualquer link
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
         mobileMenu.classList.remove('open');
-    } else {
-        mobileMenu.classList.add('open');
-    }
+    });
 });
