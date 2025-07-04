9e780e869ae153ee4958f8253b1a8bd8c0ef485f
Kawã Kleber <kawaklebersc@gmail.com>
2025-07-04 13:35:45 -0300

    Update Tailwind Css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}" // isso cobre qualquer JS dentro da src
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
